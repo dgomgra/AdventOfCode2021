7507394324a1650d940a7be474537f58efd7e849
VECTORITCGROUP\dgomez <dgomez@vectoritcgroup.com>
2021-12-30 10:13:57 +0100

    Subida de la resolución de la segunda parte del día 3 y un fichero de notas generales.

--- day3/index.js
+++ day3/index.js
@@ -48,15 +48,59 @@ console.log('TasaGamma', parseInt(tasaGamma, 2));
 console.log('TasaEpsilon:', parseInt(tasaEpsilon, 2));
 console.log('Resultado parte 1: ', parseInt(tasaGamma, 2) * parseInt(tasaEpsilon, 2));
 
-/* Parte 2*/
-const datos2 = input.split('\n');
-datos2.forEach((d) => {
-    // console.log(d);
-    const prueba = obterUno(d);
-    console.log(prueba);
-});
-
-function obterUno (l) {
-    return l;
+/* Parte 2 */
+// const datos = input.split('\n');
+part2();
+function part2() {
+    let generadorOxigeno = parseInt(getGeneradorOxigeno(datos, 0, 'GO'), 2);
+    console.log('Generador de Oxigeno:', generadorOxigeno);
+    let depuradorCO2 = parseInt(getGeneradorOxigeno(datos, 0, 'DC'), 2);
+    console.log('Generador de Oxigeno:', depuradorCO2);
+    console.log('Solución parte 2:', generadorOxigeno * depuradorCO2);
 }
+
+function getGeneradorOxigeno (lista, indice, dato) {
+    let lista1 = [];
+    let lista0 = [];
+
+    lista.forEach((element) => {
+        let caracteres = element.split('');
+        if(caracteres[indice] === '0') {
+            lista0.push(element)
+        } else {
+            lista1.push(element)
+        }
+    });
+
+    if(dato === 'GO') {
+        if(lista1.length >= lista0.length) {
+            if(lista1.length === 1) {
+                return lista1;
+            } else {
+                return getGeneradorOxigeno(lista1, indice + 1, dato);
+            }
+        } else {
+            if(lista0.length === 1) {
+                return lista0;
+            } else {
+                return getGeneradorOxigeno(lista0, indice + 1, dato);
+            }
+        }
+    } else {
+        if(lista0.length <= lista1.length) {
+            if(lista0.length === 1) {
+                return lista0;
+            } else {
+                return getGeneradorOxigeno(lista0, indice + 1, dato);
+            }
+        } else {
+            if(lista1.length === 1) {
+                return lista1;
+            } else {
+                return getGeneradorOxigeno(lista1, indice + 1, dato);
+            }
+        }
+    }
+}
+
 
